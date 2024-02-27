ba2ae68584ad2eed518c113dcc8bf2115f17289b
Aditya <158762609+devrunch@users.noreply.github.com>
2024-02-27 12:09:47 +0530

    hgfhgfvjgbk

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,7 +68,7 @@ export const Login_with_otp = async (req, res, next) => {
     });
   }
   catch (err) {
-    console.log(error)
+    console.log(err)
     res.status(400).json({
 
       type: "error",
